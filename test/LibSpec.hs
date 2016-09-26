a135651f14540691380f317d0c48d69d20478413
ocramz <zocca.marco@gmail.com>
2016-09-26 16:54:14 +0200

    FIXME solvers diverge due to SpMatrix refactoring

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -22,32 +22,39 @@ spec =
     --   True `shouldBe` True
     -- prop "ourAdd is commutative" $ \x y ->
     --   ourAdd x y `shouldBe` ourAdd y x
+    it "matVec : matrix-vector product" $
+      normSq ((aa0 #> x0true) ^-^ b0 ) <= eps `shouldBe` True
     it "BiCGSTAB" $ 
       normSq (_xBicgstab (bicgstabN aa0 b0 x0 x0 niter) ^-^ x0true) <= eps `shouldBe` True
     it "CGS" $ 
       normSq (_x (cgsN aa0 b0 x0 x0 niter) ^-^ x0true) <= eps `shouldBe` True
 
 
-
-(m,n) = (2,2)
-
-aa0 :: SpMatrix Double
-aa0 = SM (m,n) im where
-  row0 = IM.fromList [(0,1),(1,2)]
-  row1 = IM.fromList [(2,3),(3,4)]
-  im = IM.fromList [(0, row0), (1, row1)]
-
-b0, x0 :: SpVector Double
-b0 = mkSpVectorD m [8,18]
-
-x0 = mkSpVectorD m [0.3,1.4]
--- r0hat = mkSpVectorD m [1.1, 0.9]
-
 {-
+
+example 0 : 2x2 linear system
+
 [1 2] [2] = [8]
 [3 4] [3]   [18]
 
 -}
 
-x0true = mkSpVectorD n [2,3]
+aa0 :: SpMatrix Double
+aa0 = SM (2,2) im where
+  im = IM.fromList [(0, aa0r0), (1, aa0r1)]
+
+aa0r0, aa0r1 :: IM.IntMap Double
+aa0r0 = IM.fromList [(0,1),(1,2)]
+aa0r1 = IM.fromList [(2,3),(3,4)]
+
+
+-- b0, x0 : r.h.s and initial solution resp.
+b0, x0 :: SpVector Double
+b0 = mkSpVectorD 2 [8,18]
+
+x0 = mkSpVectorD 2 [0.3,1.4]
+
+
+
+x0true = mkSpVectorD 2 [2,3]
 
