cb9fb9f1e4d3ad8617c85e06ab140bbc570228d8
ocramz <zocca.marco@gmail.com>
2016-09-26 19:43:40 +0200

    FIXME compute Givens angle with Van Loan's function

--- src/Math/Linear/Sparse.hs
+++ src/Math/Linear/Sparse.hs
@@ -420,7 +420,31 @@ givens n i j theta
    s = sin theta
 
 
+{-
 
+%%%%Van Loan's Function, Chapter 7%%%%%%%%
+  function [c,s] = GivensRotation(x1,x2);
+% Pre:
+%   x1,x2   scalars
+% Post:
+%   c,s     c^2+s^2=1 so -s*x1 + c*x2 = 0.
+%
+   if x2==0
+      c = 1;
+          s = 0;
+   else
+      if abs(x2)>=abs(x1)
+             cotangent = x1/x2;
+                 s = 1/sqrt(1+cotangent^2);
+                 c = s*cotangent;
+          else
+             tangent = x2/x1;
+                 c = 1/sqrt(1+tangent^2);
+                 s = c*tangent;
+          end
+   end
+
+-}
 
 
 
