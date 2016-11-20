46480edc52055c155a1298b4f100cf53e00a48ae
ocramz <zocca.marco@gmail.com>
2016-11-20 17:54:41 +0100

    implementing CsrMatrix

--- src/Data/Sparse/Internal/CSR.hs
+++ src/Data/Sparse/Internal/CSR.hs
@@ -1,8 +1,14 @@
 module Data.Sparse.Internal.CSR where
 
+import Control.Monad.Primitive
+
+import Data.List (group, groupBy)
+
 import qualified Data.Vector as V
 import qualified Data.Vector.Unboxed as VU
-import qualified Data.Vector.Unboxed.Mutable as VM
+-- import qualified Data.Vector.Unboxed.Mutable as VM
+import qualified Data.Vector.Algorithms.Merge as VA (sort, sortBy)
+import qualified Data.Vector.Generic as VG (convert)
 
 {-| Compressed Row Storage specification :
 
@@ -25,10 +31,56 @@ import qualified Data.Vector.Unboxed.Mutable as VM
 -}
 
 data CsrMatrix a =
-  CsrMatrix { csrVal :: VU.Vector a,
-              csrColInd :: VU.Vector Int,
-              csrRowPtr :: VU.Vector Int,
-              csrNnz :: {-# UNPACK #-} !Int,
-              csrNrows :: {-# UNPACK #-} !Int,
-              csrNcols :: {-# UNPACK #-} !Int } deriving Eq
+  CsrMatrix {
+      csrNrows :: {-# UNPACK #-} !Int,
+      csrNcols :: {-# UNPACK #-} !Int,
+      csrNnz :: {-# UNPACK #-} !Int,
+      csrColVal :: V.Vector (Int, a),
+      csrRowPtr :: V.Vector Int } deriving Eq
 
+instance Show a => Show (CsrMatrix a) where
+  show (CsrMatrix m n nz cv rp) = unwords ["CSR", szs, ",",nzs] where
+    szs = unwords ["(",show m, "x", show n,")"]
+    nzs = unwords [show nz, " NZ"]
+
+toCSR :: Int -> Int -> V.Vector (Int, Int, a) -> CsrMatrix a
+toCSR m n ijxv = CsrMatrix m n nz cv crp where
+  ijxv' = sortByRows ijxv -- merge sort over row indices ( O(log N) )
+  ll = V.fromList .
+       groupBy (\x y -> fst3 x == fst3 y) . -- groupBy ( O(N) )
+       V.toList $ ijxv'
+  cv = V.map tail3 ijxv'  -- map ( O(N) )
+  crp = V.scanl (\x vs -> x + length vs) 0 ll -- scanl * length
+  nz = V.length ijxv'
+  sortByRows = V.modify (VA.sortBy f) where
+    f x y = compare (fst3 x) (fst3 y)
+  tail3 (_,j,x) = (j,x)
+  fst3 (i, _, _) = i
+
+extractRow :: CsrMatrix a -> Int -> V.Vector a
+extractRow (CsrMatrix _ _ _ cv rp) irow = V.map snd vals where
+  imin = rp V.! irow
+  imax = (rp V.! (irow + 1)) - 1
+  vals = V.drop imin $ V.take (imax + 1) cv
+
+
+
+
+
+m0 = toCSR 2 2 $ V.fromList [(0,0, pi), (1,0,3), (1,1,2)]
+
+
+
+safe :: (a -> Bool) -> (a -> b) -> a -> Maybe b
+safe q f v
+  | q v = Just (f v)
+  | otherwise = Nothing
+
+vectorNonEmpty :: (V.Vector a1 -> a) -> V.Vector a1 -> Maybe a
+vectorNonEmpty = safe (not . V.null)
+
+safeHead :: V.Vector a -> Maybe a
+safeHead = vectorNonEmpty V.head
+
+safeTail :: V.Vector a -> Maybe (V.Vector a)
+safeTail = vectorNonEmpty V.tail
