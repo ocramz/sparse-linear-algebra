a958302576f73d41c5cca652eb11ad3707fe7c6c
ocramz <zocca.marco@gmail.com>
2016-11-23 12:02:11 +0100

    added CsrVector instances for  AdditiveGroup, VectorSpace and Hilbert

--- src/Data/Sparse/Internal/CSR.hs
+++ src/Data/Sparse/Internal/CSR.hs
@@ -1,3 +1,4 @@
+{-# language TypeFamilies, FlexibleInstances, CPP #-}
 module Data.Sparse.Internal.CSR where
 
 import Control.Applicative
@@ -14,6 +15,8 @@ import qualified Data.Vector.Algorithms.Merge as VA (sortBy)
 -- import qualified Data.Vector.Generic as VG (convert)
 
 import Data.Complex
+import Foreign.C.Types (CSChar, CInt, CShort, CLong, CLLong, CIntMax, CFloat, CDouble)
+
 
 import Data.Sparse.Utils
 import Data.Sparse.Types
@@ -57,15 +60,15 @@ instance Foldable CsrMatrix where
   foldr f z (CM _ _ _ _ _ x) = foldr f z x
 
 instance Show a => Show (CsrMatrix a) where
-  show mm@(CM m n nz cix rp x) = szs where
+  show (CM m n nz cix rp x) = szs where
     szs = unwords ["CSR (",show m, "x", show n,"),",show nz, "NZ:","column indices:",show cix,", row pointers:", show rp,", data:",show x]
 
 toCSR :: Int -> Int -> V.Vector (Int, Int, a) -> CsrMatrix a
 toCSR m n ijxv = CM m n nz cix crp x where
   ijxv' = sortByRows ijxv -- merge sort over row indices ( O(log N) )
   nz = V.length ijxv'  
-  (rp, cix, x) = V.unzip3 ijxv'
-  crp = csrPtrVM m rp        -- scanl + replicate + takeWhile * map ( O(N) )
+  (rix, cix, x) = V.unzip3 ijxv'
+  crp = csrPtrVM m rix        -- scanl + replicate + takeWhile * map ( O(N) )
   sortByRows = V.modify (VA.sortBy f) where
        f a b = compare (fst3 a) (fst3 b)
   csrPtrVM nrows xs = V.scanl (+) 0 $ V.create createf where
@@ -79,8 +82,6 @@ toCSR m n ijxv = CM m n nz cix crp x where
                                      loop v (V.drop lp ll) (i + 1)
      loop vm xs 0
      return vm
-
-
 
 
      
@@ -101,14 +102,8 @@ lookupRow cm i | null er = Nothing
 extractRow :: CsrMatrix a -> IxRow -> CsrVector a
 extractRow (CM m n _ cc rp x) irow = CV n ixs vals where
   imin = rp V.! irow
-  imax = (rp V.! (irow + 1)) - 1
-  ixs = V.slice imin imax cc -- V.drop imin $ V.take (imax + 1) cv
-  vals = V.slice imin imax x
-
-extractRow' (CM m n _ cc rp x) irow = (imin, imax, ixs, vals)where
-  imin = rp V.! irow
-  imax = (rp V.! (irow + 1)) - 1
-  ixs = V.slice imin imax cc -- V.drop imin $ V.take (imax + 1) cv
+  imax = rp V.! (irow + 1)
+  ixs = V.slice imin imax cc 
   vals = V.slice imin imax x
 
 
@@ -135,7 +130,7 @@ data CsrVector a = CV { cvDim :: {-# UNPACK #-} !Int,
                         cvVal :: V.Vector a } deriving Eq
 
 instance Show a => Show (CsrVector a) where
-  show (CV n ix v) = unwords ["CV (",show n,"),",show nz,"NZ:",show v]
+  show (CV n ix v) = unwords ["CV (",show n,"),",show nz,"NZ:",show (V.zip ix v)]
     where nz = V.length ix
 
 instance Functor CsrVector where
@@ -147,12 +142,51 @@ instance Foldable CsrVector where
 instance Traversable CsrVector where
   traverse f (CV n ix v) = CV n ix <$> traverse f v
 
+
+
+#define CVType(t) \
+  instance AdditiveGroup (CsrVector t) where {zero = CV 0 V.empty V.empty; (^+^) = unionWithCV (+) 0 ; negated = fmap negate};\
+  instance VectorSpace (CsrVector t) where {type Scalar (CsrVector t) = (t); n .* x = fmap (* n) x };\
+  instance Hilbert (CsrVector t) where {x `dot` y = sum $ intersectWithCV (*) x y };\
+
+#define CVTypeC(t) \
+  instance AdditiveGroup (CsrVector (Complex t)) where {zero = CV 0 V.empty V.empty; (^+^) = unionWithCV (+) (0 :+ 0) ; negated = fmap negate};\
+  instance VectorSpace (CsrVector (Complex t)) where {type Scalar (CsrVector (Complex t)) = (Complex t); n .* x = fmap (* n) x };\
+  instance Hilbert (CsrVector (Complex t)) where {x `dot` y = sum $ intersectWithCV (*) (fmap conjugate x) y};\
+
+CVType(Int)
+CVType(Integer)
+CVType(Float)
+CVType(Double)
+CVType(CSChar)
+CVType(CInt)
+CVType(CShort)
+CVType(CLong)
+CVType(CLLong)
+CVType(CIntMax)
+CVType(CFloat)
+CVType(CDouble)
+
+CVTypeC(Float)
+CVTypeC(Double)  
+CVTypeC(CFloat)
+CVTypeC(CDouble)
+
+
+
+
 -- ** Construction 
 
-fromDenseV :: V.Vector a -> CsrVector a
-fromDenseV xs = CV n (V.enumFromTo 0 (n-1)) xs where
+fromDenseCV :: V.Vector a -> CsrVector a
+fromDenseCV xs = CV n (V.enumFromTo 0 (n-1)) xs where
   n = V.length xs
 
+fromVectorCV :: Int -> V.Vector (Int, a) -> CsrVector a
+fromVectorCV n ixs = CV n ix xs where
+  (ix, xs) = V.unzip ixs
+
+fromListCV :: Int -> [(Int, a)] -> CsrVector a
+fromListCV n = fromVectorCV n . V.fromList
 
 -- * Query
 
@@ -185,27 +219,63 @@ intersectWith f g u_ v_ = V.force $ V.create $ do
   let vm'' = VM.take i' vm'
   return vm''
 
--- unionWith f g u_ v_ = V.force $ V.create $ do
---   let n = max (V.length u_) (V.length v_)
---   vm <- VM.new n
---   let go u_ v_ i vm | i == n = return (vm, i)
---                     | V.null u_ && V.length v_ == 1 = do
---                         VM.write vm i (g u v)
---                     | otherwise =  do
---          let (u,us) = (V.head u_, V.tail u_)
---              (v,vs) = (V.head v_, V.tail v_)
---          if f u == f v then do VM.write vm i (g u v)
---                                go us vs (i + 1) vm
---                    else if f u < f v then go us v_ i vm
---                                      else go u_ vs i vm
---   (vm', i') <- go u_ v_ 0 vm
---   let vm'' = VM.take i' vm'
---   return vm''
+
+-- | O(N) Applies a function to the index _intersection_ of two CsrVector s
+intersectWithCV :: (t -> t1 -> a) -> CsrVector t -> CsrVector t1 -> CsrVector a
+intersectWithCV g (CV n1 ixu_ uu_) (CV n2 ixv_ vv_) = CV nfin ixf vf where
+   nfin = V.length vf
+   (ixf, vf) = V.unzip $ V.force $ V.create $ do
+    let n = max n1 n2
+    vm <- VM.new n
+    let go ixu u_ ixv v_ i vm | V.null u_ || V.null v_ || i == n = return (vm, i)
+                              | otherwise =  do
+           let (u,us) = (V.head u_, V.tail u_)
+               (v,vs) = (V.head v_, V.tail v_)
+               (ix1,ix1s) = (V.head ixu, V.tail ixu)
+               (ix2,ix2s) = (V.head ixv, V.tail ixv)
+           if ix1 == ix2 then do VM.write vm i (ix1, g u v)
+                                 go ix1s us ix2s vs (i + 1) vm
+                     else if ix1 < ix2 then go ix1s us ixv v_ i vm
+                                       else go ixu u_ ix2s vs i vm
+    (vm', nfin) <- go ixu_ uu_ ixv_ vv_ 0 vm
+    let vm'' = VM.take nfin vm'
+    return vm''
+
+unionWithCV :: (t -> t -> a) -> t -> CsrVector t -> CsrVector t -> CsrVector a
+unionWithCV g z (CV n1 ixu_ uu_) (CV n2 ixv_ vv_) = CV n ixf vf where
+   n = max n1 n2
+   (ixf, vf) = V.unzip $ V.force $ V.create $ do
+    vm <- VM.new n
+    let go iu u_ iv v_ i vm
+          | (V.null u_ && V.null v_) || i == n = return (vm , i)
+          | V.null u_ = do
+                 VM.write vm i (V.head iv, g z (V.head v_))
+                 go iu u_ (V.tail iv) (V.tail v_) (i + 1) vm
+          | V.null v_ = do
+                 VM.write vm i (V.head iu, g (V.head u_) z)
+                 go (V.tail iu) (V.tail u_) iv v_ (i + 1) vm
+          | otherwise =  do
+           let (u,us) = (V.head u_, V.tail u_)
+               (v,vs) = (V.head v_, V.tail v_)
+               (iu1,ius) = (V.head iu, V.tail iu)
+               (iv1,ivs) = (V.head iv, V.tail iv)
+           if iu1 == iv1 then do VM.write vm i (iu1, g u v)
+                                 go ius us ivs vs (i + 1) vm
+                         else if iu1 < iv1 then do VM.write vm i (iu1, g u z)
+                                                   go ius us iv v_ (i + 1) vm
+                                           else do VM.write vm i (iv1, g z v)
+                                                   go iu u_ ivs vs (i + 1) vm
+    (vm', nfin) <- go ixu_ uu_ ixv_ vv_ 0 vm
+    let vm'' = VM.take nfin vm'
+    return vm''                                                       
+           
 
 
+
+union :: Ord a => [a] -> [a] -> [a]
 union u_ v_ = go u_ v_ where
-  go [] [b] = [b]
-  go [a] [] = [a]
+  go [] x = x
+  go y [] = y
   go uu@(u:us) vv@(v:vs)
     | u == v =    u : go us vs
     | u < v =     u : go us vv 
@@ -223,43 +293,7 @@ liftI2V f = intersectWith fst (\(i, x) (_, y) -> (i, f x y))
 
 
 
--- | Dot product, real domain
-dot :: (Ord i, Num b) => V.Vector (i, b) -> V.Vector (i, b) -> b
-dot a b = foldlIxV' (+) 0 $ liftI2V (*) a b 
 
--- | Dot product, complex domain
-dotC ::
-  (RealFloat a, Ord t) => V.Vector (t, Complex a) -> V.Vector (t, Complex a) -> a
-dotC a b = realPart $ foldlIxV' (+) 0 $ liftI2V (\x y -> conjugate x * y) a b 
-
-
-
--- instance F.Foldable IxVector where
---   foldr f z v = foldr (\(_, x) a -> f a x) z (unIxVector v)
-
-foldrIxV :: (t1 -> t2 -> t1) -> t1 -> V.Vector (t, t2) -> t1
-foldrIxV f z v = V.foldr (\(_, x) a -> f a x) z v
-
-foldlIxV' :: (t1 -> t2 -> t1) -> t1 -> V.Vector (t, t2) -> t1
-foldlIxV' f z v = V.foldl' (\b (_, x) -> f b x) z v
-
-
-
-
--- * IxVector newtype
--- newtype IxVector a = IxVector { unIxVector :: V.Vector (Int, a)} deriving (Eq, Show)
-
--- instance Functor IxVector where
---   fmap f iv = IxVector $ fmap g (unIxVector iv) where
---     g (_i, x) = (_i, f x) -- bleh
-    
--- instance Set IxVector where
---   liftI2 = liftI2IxV
-
--- liftI2IxV :: (a -> a -> b) -> IxVector a -> IxVector a -> IxVector b
--- liftI2IxV f a b = IxVector $ liftI2V f a' b' where
---   a' = unIxVector a
---   b' = unIxVector b
 
   
 
@@ -293,9 +327,13 @@ v0,v1 :: V.Vector Int
 v0 = V.fromList [0,1,2,5,6]
 v1 = V.fromList [0,3,4,6]
 
-e1, e2 :: V.Vector (Int, Double)
-e1 = V.indexed $ V.fromList [1,0,0]
-e2 = V.indexed $ V.fromList [0,1,0]
+-- e1, e2 :: V.Vector (Int, Double)
+-- e1 = V.indexed $ V.fromList [1,0,0]
+-- e2 = V.indexed $ V.fromList [0,1,0]
+
+e1 = fromListCV 3 [(0, 1)]
+e2 = fromListCV 3 [(1, 1)]
+e3 = fromListCV 3 [(0, 1 :+ 2)] :: CsrVector (Complex Double)
 
 e1c = V.indexed $ V.fromList [1,0,0] :: V.Vector (Int, Complex Double)
 
