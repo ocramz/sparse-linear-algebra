dd24c7d41096b514fb59b1c31d7e215304db8e08
ocramz <zocca.marco@gmail.com>
2016-11-30 16:12:10 +0100

    moved SpMatrix1 to CSR module

--- src/Data/Sparse/Internal/CSR.hs
+++ src/Data/Sparse/Internal/CSR.hs
@@ -81,7 +81,7 @@ toCSR :: Int -> Int -> V.Vector (Int, Int, a) -> CsrMatrix a
 toCSR m n ijxv = CM m n nz cix crp x where
   nz = V.length x
   (rix, cix, x) = V.unzip3 (sortByRows ijxv)
-  crp = csrPtrV m rix
+  crp = csrPtrV (==) m rix
   sortByRows = V.modify (VA.sortBy f) where
        f a b = compare (fst3 a) (fst3 b)
 
@@ -157,15 +157,6 @@ transposeCSR mm = toCSR n m $ V.zip3 jj ii xx where
 
 
 
-
--- matVec mm v = fmap (\ri -> extractRow mm ri `dot` v) irows_ where
---   nrows = csrNrows mm
---   irows_ = [0 .. nrows - 1]
-
-
-
-
-
 -- some instances
 
 instance FiniteDim CsrMatrix where
@@ -189,19 +180,132 @@ instance Elt a => SpContainer CsrMatrix a where
 
 
 
+
+
+
+
+
+-- * SpMatrix1 : entry coordinates are stored in lexicographic encodeding
+
+data SpMatrix1 a = SM1 { smNrows :: Rows,
+                         smNcols :: Cols,
+                         smLexOrd :: LexOrd,
+                         smPtr :: V.Vector Int,
+                         smDat :: V.Vector (LexIx, a) } deriving (Eq, Show)
+
+instance Functor SpMatrix1 where
+  fmap f (SM1 r c et p x) = SM1 r c et p (fmap g x) where
+    g (i, v) = (i, f v)
+
+instance FiniteDim SpMatrix1 where
+  type FDSize SpMatrix1 = (Rows, Cols)
+  dim x = (smNrows x, smNcols x)
+
+instance HasData SpMatrix1 a where
+  type HDData SpMatrix1 a = V.Vector (LexIx, a)
+  dat = smDat
+  nnz = V.length . dat
+
+instance Sparse SpMatrix1 a where
+  spy mm = fromIntegral (nnz mm) / fromIntegral (m * n) where (m, n) = dim mm
+
+instance Num a => SpContainer SpMatrix1 a where
+  type ScIx SpMatrix1 = LexIx
+  scToList = V.toList . fmap snd . dat
+  scLookup = smLookup
+  mm @@ i = fromMaybe 0 (scLookup mm i)
+
+smLookup :: SpMatrix1 a -> LexIx -> Maybe a
+smLookup mm i = snd <$> V.find ((== i) . fst) (dat mm)
+
+
+
+
+
+
+-- ** Encoding/decoding between coordinate representation and lexicographic index
+-- | Coordinates to lexicographic 
+encode :: LexOrd -> Rows -> Cols -> (IxRow, IxCol) -> LexIx
+encode t 
+  | t == RowsFirst = encodeR
+  | otherwise = encodeC where
+      encodeR m _ (i, j) = m * j + i
+      encodeC _ n (i, j) = n * i + j
+
+-- | Lexicographic  to coordinates
+decode :: LexOrd -> Rows -> Cols -> LexIx -> (IxRow, IxCol)
+decode t
+  | t == RowsFirst = decodeR
+  | otherwise = decodeC where
+      decodeR m _ ii = (i, j) where
+        (j, i) = divMod ii m
+      decodeC _ n jj = divMod jj n
+
+
+instance Num a => SparseMatrix SpMatrix1 a where
+  smFromVector et (m, n) v = SM1 m n et p' v' where
+    enc (i, j, x) = (encode et m n (i, j), x) -- encode indices
+    v' = sortByIx $ V.map enc v
+    p' = csrPtrVTup et m n v'
+  encodeIx mm o = encode o m n where (m, n) = dim mm
+  decodeIx mm o = decode o m n where (m, n) = dim mm
+--   smTranspose ColsFirst = transposeSM1
+
+
+-- | Generate the row/column row pointer according to the respective encoding
+csrPtrVTup ::
+  LexOrd -> Rows -> Cols -> V.Vector (LexIx, t) -> V.Vector Int
+csrPtrVTup et m n v
+  | et == RowsFirst = csrPtrV (\(i, _) i0 -> fst (dec i) == i0) m v
+  | otherwise       = csrPtrV (\(i, _) i0 -> snd (dec i) == i0) n v 
+    where
+      dec = decode et m n
+      
+
+
+
+
+-- NB : binary operations on SpMatrix1 are defined only if dimensions AND lexical order encoding are equal
+instance Num a => AdditiveGroup (SpMatrix1 a) where
+  -- zero = SM1 0 0 ColsFirst V.empty V.empty
+  negated = fmap negate
+
+-- stream merge -> SpMatrix1 binary lift
+
+
+
+
+
+-- -- | Transpose: remap keys ( O(N) ) followed by merge sort ( O(log N) )
+-- transposeSM1 :: Num a => SpMatrix1 a -> SpMatrix1 a
+-- transposeSM1 mm = SM1 n m p' d' where
+--   fi (i, j) = (j, i)
+--   (m, n) = dim mm
+--   d' = sortByIx $ fmapIx (remapCoords fi mm) (dat mm)
+--   p' = csrPtrV' (\(i, x) i0 -> i==i0) n d'
+
+remapCoords ::
+  SparseMatrix m e => ((IxRow, IxCol) -> (IxRow, IxCol)) -> m e -> LexIx -> LexIx
+remapCoords fi mm = encodeIx mm ColsFirst . fi . decodeIx mm ColsFirst
+
+
+-- | Map a function over the indices (internal use only)
+fmapIx :: Functor f => (a -> b) -> f (a, t) -> f (b, t)
+fmapIx f tt = fmap g tt where g (i, x) = (f i, x)
+
+-- | Sort according to index 
+sortByIx :: V.Vector (LexIx, b) -> V.Vector (LexIx, b)
+sortByIx = V.modify (VA.sortBy f) where
+  f x y = compare (fst x) (fst y)
+
+
+
+
+
+
+
                                                  
-           
-
-
-
-
-  
-
-
-
-
-
-
+          
 
 -- * Utilities
 
@@ -214,6 +318,30 @@ fst3 :: (t, t1, t2) -> t
 fst3 (i, _, _) = i
 
 
+
+
+
+
+
+-- test data
+v0 :: V.Vector (Int, Double)
+v0 = V.fromList [(0, pi), (2, 3), (3, 57)]
+
+v1 = V.fromList [(0,0,pi), (0,1,5), (2,1,exp 1)]
+
+sm0 = smFromVector ColsFirst (3, 3) v1 :: SpMatrix1 Double
+
+
+
+
+v2r, v2c :: V.Vector (LexIx, Double)
+v2r = V.fromList [(1, 5), (5, 8), (7, 6), (10, 3)]
+
+v2c = V.fromList [(4, 5), (5, 8), (10, 3), (13, 6)]
+
+v2 :: V.Vector (Int, Int, Double)
+v2 = V.fromList [(1,0,5), (1,1,8), (2,2,3), (3,1,6)]
+sm2 = smFromVector RowsFirst (4, 4) v2 :: SpMatrix1 Double
 
 
 
