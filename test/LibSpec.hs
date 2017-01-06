2291b007521c4a391d41eb887ceeadd1fb3146ec
ocramz <zocca.marco@gmail.com>
2017-01-06 12:11:52 +0100

    WORKING

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleContexts, TypeFamilies #-}
 {-# language ScopedTypeVariables #-}
 -----------------------------------------------------------------------------
 -- |
