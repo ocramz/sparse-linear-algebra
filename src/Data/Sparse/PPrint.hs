eef28b83c6086ac6a3e5f15b88e2348cb26ca816
ocramz <zocca.marco@gmail.com>
2017-02-20 15:38:10 +0100

    fixing PPrint

--- src/Data/Sparse/PPrint.hs
+++ src/Data/Sparse/PPrint.hs
@@ -32,10 +32,12 @@ data PPrintOptions =
      pprintDigits :: Int,
      pprintDecimals :: Int } deriving (Eq, Show)
 
+pprintDefaults :: PPrintOptions
 pprintDefaults = PPOpts 1 3
 
 
-
+-- | printf format string for a Fractional
+prepD :: (Ord t, Fractional t) => PPrintOptions -> t -> String
 prepD opts x = pstr -- printf pstr x
   where
   pstr | abs x > 10 || abs x < 0.5 = s ++ "e"
@@ -45,10 +47,8 @@ prepD opts x = pstr -- printf pstr x
   nd = pprintDecimals opts
   ni = pprintDigits opts
 
-
-
--- printfComplex :: (PrintfArg t, Epsilon t, Ord t) =>
---      PPrintOptions -> Complex t -> String
+-- | printf format string for a Complex Fractional
+prepC :: (Epsilon t, Ord t) => PPrintOptions -> Complex t -> String
 prepC opts (r :+ i) = prepD opts r ++ oi where
     oi | isNz i = concat [s, "i ", prepD opts i']
        | otherwise = []
@@ -56,18 +56,6 @@ prepC opts (r :+ i) = prepD opts r ++ oi where
       | otherwise = " - "
     i' = abs i
 
--- | Cleaner way to display Complex values
 
-newtype C a = C {unC :: Complex a} deriving Eq
-instance (Num a, Epsilon a, Ord a, Show a) => Show (C a) where
-  show (C (r :+ i)) = unwords [show r, oi] where
-    oi | isNz i = unwords [s, show i' ++ "j"]
-       | otherwise = []
-    s | signum i >= 0 = "+"
-      | otherwise = "-"
-    i' = abs i
 
--- c0, c1 :: C Double
--- c0 = C $ 1 :+ (-2) 
--- c1 = C $ pi :+ 0
 
