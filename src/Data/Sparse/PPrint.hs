1bf501f259933b63b215a1ce0d640865d1426842
ocramz <zocca.marco@gmail.com>
2017-02-20 20:04:39 +0100

    printf is not fun to use

--- src/Data/Sparse/PPrint.hs
+++ src/Data/Sparse/PPrint.hs
@@ -10,6 +10,7 @@
 module Data.Sparse.PPrint where
 
 import Data.Complex
+import Data.List
 import Text.Printf
 
 import Numeric.Eps
@@ -29,23 +30,26 @@ newline = putStrLn ""
 
 data PPrintOptions =
   PPOpts {
-     pprintDigits :: Int,
-     pprintDecimals :: Int } deriving (Eq, Show)
+     pprintInt :: Int,
+     pprintDec :: Int
+     } deriving (Eq, Show)
 
 pprintDefaults :: PPrintOptions
 pprintDefaults = PPOpts 1 3
+
+
 
 
 -- | printf format string for a Fractional
 prepD :: (Ord t, Fractional t) => PPrintOptions -> t -> String
 prepD opts x = pstr -- printf pstr x
   where
-  pstr | abs x > 10 || abs x < 0.5 = s ++ "e"
+  pstr | abs x >= 10 || abs x < 0.5 = s ++ "e"
        | otherwise = s ++ "f"
     where
       s = concat ["%" , show ni, ".", show nd]
-  nd = pprintDecimals opts
-  ni = pprintDigits opts
+  nd = pprintDec opts
+  ni = pprintInt opts
 
 -- | printf format string for a Complex Fractional
 prepC :: (Epsilon t, Ord t) => PPrintOptions -> Complex t -> String
@@ -58,4 +62,21 @@ prepC opts (r :+ i) = prepD opts r ++ oi where
 
 
 
-
+printN prepf opts xl
+  | null xl = printf "\n"
+  | null xs = printf (prepf opts x) x
+  | n==1 = let [x1]=xs in printf s x x1
+  | n==2 = let [x1,x2]=xs in printf s x x1 x2
+  | n==3 = let [x1,x2,x3]=xs in printf s x x1 x2 x3
+  | n==4 = let [x1,x2,x3,x4]=xs in printf s x x1 x2 x3 x4
+  | otherwise = let [x1,x2,x3,x4,_]=xs
+                    xfin=last xs in printf s' x x1 x2 x3 x4 xfin
+  where
+    (x:xs) = xl
+    n = length xs
+    s = unwords (replicate (n+1) (prepf opts x)) ++ "\n"
+    s'= unwords [unwords (replicate n (prepf opts x)), "...", prepf opts x] ++ "\n"
+  -- case length ll of 1 -> printf (prepf opts x) x where
+  --                     x = head ll
+  --                   2 -> printf (prepf opts )
+  
