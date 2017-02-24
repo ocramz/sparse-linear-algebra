547a47fbc4ccfbe3444ef6ec5890d11869d122a7
ocramz <zocca.marco@gmail.com>
2017-02-24 11:45:50 +0100

    FIXED PPring

--- src/Data/Sparse/PPrint.hs
+++ src/Data/Sparse/PPrint.hs
@@ -7,7 +7,7 @@
 -- Portability :  portable
 --
 -----------------------------------------------------------------------------
-module Data.Sparse.PPrint where
+module Data.Sparse.PPrint (prd, prd0, PPrintOptions, prdef, prepD, prepC, printDN, printCN) where
 
 import Data.Complex
 import Data.List
@@ -34,8 +34,8 @@ data PPrintOptions =
      pprintDec :: Int
      } deriving (Eq, Show)
 
-pdef :: PPrintOptions
-pdef = PPOpts 1 3
+prdef :: PPrintOptions
+prdef = PPOpts 1 3
 
 
 
@@ -44,7 +44,7 @@ pdef = PPOpts 1 3
 prepD :: (Ord t, Fractional t) => PPrintOptions -> t -> String
 prepD opts x = pstr -- printf pstr x
   where
-  pstr | abs x >= 10 || abs x < 0.5 = s ++ "e"
+  pstr | abs x >= 10 || abs x < 0.1 = s ++ "e"
        | otherwise = s ++ "f"
     where
       s = concat ["%" , show ni, ".", show nd]
@@ -64,16 +64,19 @@ prepC opts (r :+ i) = prepD opts r ++ oi where
 -- | printf for a list of values
 --
 -- > printN prepD (PPOpts 1 3) [1,pi]
+printDN
+  :: (PrintfArg t1, PrintfType t, Ord t1, Fractional t1) =>
+     PPrintOptions -> [t1] -> t
 printDN opts xl
   | null xl = printf "\n"
   | null xs = printf (prepD opts x) x
-  | n==1 = let [x1]=xs in printf (pr x) x
-  -- | n==1 = let [x1]=xs in printf s x x1
-  -- | n==2 = let [x1,x2]=xs in printf s x x1 x2
-  -- | n==3 = let [x1,x2,x3]=xs in printf s x x1 x2 x3
-  -- | n==4 = let [x1,x2,x3,x4]=xs in printf s x x1 x2 x3 x4
-  -- | otherwise = let [x1,x2,x3,x4,_]=xs
-  --                   xfin=last xs in printf s' x x1 x2 x3 x4 xfin
+  | n==1 = let [x1]=xs in printf (commas (pr <$> xl)++"\n") x x1
+  | n==2 = let [x1,x2]=xs in printf (commas (pr <$> xl)++"\n") x x1 x2
+  | n==3 = let [x1,x2,x3]=xs in printf (commas (pr <$> xl)++"\n") x x1 x2 x3
+  | n==4 = let [x1,x2,x3,x4]=xs in printf (commas (pr <$> xl)++"\n") x x1 x2 x3 x4
+  | otherwise = let xs@[x,x1,x2,x3]=take 4 xl
+                    xfin=last xl
+                in printf (commas (pr <$> xs) ++ ", ... , " ++ pr xfin++"\n") x x1 x2 x3 xfin
   where
     (x:xs) = xl
     n = length xs
@@ -81,7 +84,10 @@ printDN opts xl
 
 
 
--- > printCN pdef [(1:+pi), (3.5:+4.3), (pi:+(-3.4))]     
+-- > printCN pdef [(1:+pi), (3.5:+4.3), (pi:+(-3.4))]
+printCN
+  :: (PrintfArg t1, PrintfType t, Epsilon t1, Ord t1) =>
+     PPrintOptions -> [Complex t1] -> t
 printCN opts xl
   | null xl = printf "\n"
   | null xs = printf (pr x) (re x) (aim x)
