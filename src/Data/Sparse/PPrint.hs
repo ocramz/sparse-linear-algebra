0a5ab92a9b49bc9a2546f304f64c39893c270197
ocramz <zocca.marco@gmail.com>
2017-03-03 17:01:34 +0100

    I do hate printf

--- src/Data/Sparse/PPrint.hs
+++ src/Data/Sparse/PPrint.hs
@@ -1,4 +1,4 @@
-{-# language FlexibleContexts #-}
+{-# language FlexibleContexts, RecordWildCards #-}
 -----------------------------------------------------------------------------
 -- |
 -- Copyright   :  (C) 2016 Marco Zocca
@@ -9,9 +9,9 @@
 --
 -----------------------------------------------------------------------------
 module Data.Sparse.PPrint (prd, prd0, PrintDense, newline,
-                           PPrintOptions, prdef, prepD, prepC,
-                           -- printDN,
-                           printCN) where
+                           PPrintOptions, prdef, prepD, prepC
+                           , printDN , printCN
+                          ) where
 
 import Data.Complex
 import Data.List
@@ -34,36 +34,43 @@ newline = putStrLn ""
 -- | Pretty printing options: # of integer digits, # of decimal digits
 data PPrintOptions =
   PPOpts {
-     pprintInt :: Int,
-     pprintDec :: Int
+     pprintInt, pprintDec, pprintColWidth :: Int
      } deriving (Eq, Show)
 
 prdef :: PPrintOptions
-prdef = PPOpts 1 2
+prdef = PPOpts 1 2 6
 
 
 
 
 -- | printf format string
-prepD :: (Ord t, Epsilon t) => PPrintOptions -> t -> String
-prepD opts x = pstr -- printf pstr x
-  where
-  pstr | nearZero x = "_"
-       | abs x > 999 || abs x < 0.1 = s ++ "e"
-       | otherwise = s ++ "f"
-    where
-      s = concat ["%" , show ni, ".", show nd]
-  nd = pprintDec opts
-  ni = pprintInt opts
+prepD :: (Ord t, Epsilon t) => Bool -> PPrintOptions -> t -> String
+prepD ptn PPOpts{..} x = s ++ s2 where
+  s = concat ["%" , show ni, ".", show nd]
+  s2 | ptn = pstr x
+     | otherwise = pstr0 x
+  s3 = show tabw
+  nd = pprintDec 
+  ni = pprintInt
+  tabw = pprintColWidth
+  
+pstr0 :: (Epsilon a, Ord a) => a -> String
+pstr0 x | nearZero x = "_"
+        | otherwise = pstr x
+pstr :: (Ord a, Fractional a) => a -> String          
+pstr x | abs x > 999 || abs x < 0.1 = "e"
+       | otherwise = "f"                
+
 
 -- | printf format string for a Complex
 prepC :: (Epsilon t, Ord t) => PPrintOptions -> Complex t -> String
-prepC opts (r :+ i) = prepD opts r ++ oi where
-    oi | isNz i = concat [s, prepD opts i'," i"]
-       | otherwise = []
-    s | signum i >= 0 = " + "
-      | otherwise = " - "
-    i' = abs i
+prepC opts (r :+ i) = prepD True opts r ++ oi where
+  oi = concat [s, prepD True opts i', " i"]
+  s | signum i >= 0 = " + "
+    | otherwise = " - "
+  i' = abs i
+  
+
 
 
 -- | printf for a list of real values
@@ -79,7 +86,7 @@ printDN opts xl0
   | n==4 = let [x1,x2,x3,x4]=nums in printf strsc x1 x2 x3 x4
   where
     nmax = 4
-    (n, strs, nums) = printN opts prepD nmax xl0
+    (n, strs, nums) = printN opts (prepD False) nmax xl0
     strsc = commas strs ++ "\n"
     -- pr = prepD opts
 
@@ -122,21 +129,7 @@ printN opts prepf nmax xl0 = go 0 xl0 [] [] where
   go nfin [] ss ns = (nfin, reverse ss , reverse ns)
 
 
-printN_C opts prepf nmax xl0 = go 0 xl0 [] [] where
-  pr = prepf opts
-  go i [x] ss ns | isNz x = go (i+1) [] (pr x : ss) (x : ns)
-                 | otherwise = go i [] ("_" : ss) ns  
-  go i (x:xs) ss ns | isNz x = if i < nmax - 2
-                               then go (i+1) xs (pr x : ss) (x : ns)
-                               else if i == nmax - 2
-                                    then go (i+1) xs (" ... " : pr x : ss) (x : ns)
-                                    else go i xs ss ns
-                    | otherwise = if i < nmax - 2 
-                                  then go i xs ("_" : ss) ns
-                                  else if i == nmax - 2 
-                                       then go (i+1) xs (" ... " : "_" : ss) ns
-                                       else go i xs ss ns
-  go nfin [] ss ns = (nfin, reverse ss , reverse ns)  
+
 
   
 
