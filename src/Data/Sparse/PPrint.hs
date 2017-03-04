1289003cc20446d273a21113b8f6958053cf1afc
ocramz <zocca.marco@gmail.com>
2017-03-04 15:55:03 +0100

    FIXED PPrint

--- src/Data/Sparse/PPrint.hs
+++ src/Data/Sparse/PPrint.hs
@@ -31,14 +31,28 @@ newline :: IO ()
 newline = putStrLn ""  
 
 
--- | Pretty printing options: # of integer digits, # of decimal digits
+-- | Pretty printing options: total length in # digits (including the decimal point), # of decimal digits
 data PPrintOptions =
   PPOpts {
-     pprintInt, pprintDec, pprintColWidth :: Int
+     pprintLen, pprintDec, pprintColWidth :: Int
      } deriving (Eq, Show)
 
 prdef :: PPrintOptions
-prdef = PPOpts 1 2 6
+prdef = PPOpts 4 2 15
+
+
+
+-- | Pretty print an array of real numbers
+printDN :: (PrintfArg a, Epsilon a, Foldable f, Ord a) =>
+     PPrintOptions -> f a -> String
+printDN opts = printNpad f opts where
+  f o x = printf (prepD True o x) x
+
+-- | Pretty print an array of complex numbers
+printCN :: (PrintfArg a, Epsilon a, Foldable f, Ord a) =>
+     PPrintOptions -> f (Complex a) -> String
+printCN opts = printNpad f opts where
+  f o x = printf (prepC o x) (re x) (aim x)
 
 
 
@@ -49,96 +63,44 @@ prepD ptn PPOpts{..} x = s ++ s2 where
   s = concat ["%" , show ni, ".", show nd]
   s2 | ptn = pstr x
      | otherwise = pstr0 x
-  s3 = show tabw
   nd = pprintDec 
-  ni = pprintInt
-  tabw = pprintColWidth
-  
-pstr0 :: (Epsilon a, Ord a) => a -> String
-pstr0 x | nearZero x = "_"
-        | otherwise = pstr x
-pstr :: (Ord a, Fractional a) => a -> String          
-pstr x | abs x > 999 || abs x < 0.1 = "e"
-       | otherwise = "f"                
+  ni = pprintLen
+  pstr0 x | nearZero x = "_"
+          | otherwise = pstr x
+  pstr x | abs x > 999 || abs x < 0.1 = "e"
+         | otherwise = "f"                
 
 
 -- | printf format string for a Complex
 prepC :: (Epsilon t, Ord t) => PPrintOptions -> Complex t -> String
 prepC opts (r :+ i) = prepD True opts r ++ oi where
-  oi = concat [s, prepD True opts i', " i"]
+  oi = concat [s, prepD True opts i', "i"]
   s | signum i >= 0 = " + "
     | otherwise = " - "
   i' = abs i
 
 
- 
 
+printNpad :: Foldable f =>
+     (PPrintOptions -> a -> String) -> PPrintOptions -> f a -> String
+printNpad f o@PPOpts{..} xl = concat $ foldr ins [] xl where
+  ins x acc = (s ++ spad) : acc where
+    n = pprintColWidth
+    s = f o x
+    spad = spaces (n - length s)
 
--- | printf for a list of real values
---
--- > printDN (PPOpts 1 3) 2 [1,pi]
-printDN :: (PrintfArg a, PrintfType t, Epsilon a, Ord a) =>
-     PPrintOptions -> [a] -> t
-printDN opts xl0
-  | n==0 = printf "\n"
-  | n==1 = let [x1]=nums in printf strsc x1
-  | n==2 = let [x1,x2]=nums in printf strsc x1 x2
-  | n==3 = let [x1,x2,x3]=nums in printf strsc x1 x2 x3
-  | n==4 = let [x1,x2,x3,x4]=nums in printf strsc x1 x2 x3 x4
-  where
-    nmax = 4
-    (n, strs, nums) = printN opts (prepD False) nmax xl0
-    strsc = commas strs ++ "\n"
-    -- pr = prepD opts
+    
 
+-- | Helpers
 
--- | printf for list of complex values
--- 
--- > printCN prdef [(1:+pi), (3.5:+4.3), (pi:+(-3.4))]
-printCN :: (PrintfArg t1, PrintfType t, Epsilon (Complex t1), Epsilon t1,
-      Ord t1) =>
-     PPrintOptions -> [Complex t1] -> t
-printCN opts xl0
-  | n==0 = printf "\n"
-  | n==1 = let [x]=nums in printf strsc (re x) (aim x)
-  | n==2 = let [x1,x2]=nums in printf strsc (re x1) (aim x1) (re x2) (aim x2)
-  | n==3 = let [x1,x2,x3]=nums in printf strsc (re x1) (aim x1) (re x2) (aim x2) (re x3) (aim x3)
-  | n==4 = let [x1,x2,x3,x4]=nums in printf strsc (re x1) (aim x1) (re x2) (aim x2) (re x3) (aim x3) (re x4) (aim x4)
-  where
-    nmax = 4
-    (n, strs, nums) = printN opts prepC nmax xl0
-    strsc = commas strs ++ "\n"
-
-
-
-printN :: Epsilon a =>
-     t -> (t -> a -> String) -> Int -> [a] -> (Int, [String], [a])
-printN opts prepf nmax xl0 = go 0 xl0 [] [] where
-  pr = prepf opts
-  go i [x] ss ns | isNz x = go (i+1) [] (pr x : ss) (x : ns)
-                 | otherwise = go i [] ("_" : ss) ns  
-  go i (x:xs) ss ns | isNz x = if i < nmax - 2
-                               then go (i+1) xs (pr x : ss) (x : ns)
-                               else if i == nmax - 2
-                                    then go (i+1) xs (" ... " : pr x : ss) (x : ns)
-                                    else go i xs ss ns
-                    | otherwise = if i < nmax - 2 
-                                  then go i xs ("_" : ss) ns
-                                  else if i == nmax - 2 
-                                       then go (i+1) xs (" ... " : "_" : ss) ns
-                                       else go i xs ss ns
-  go nfin [] ss ns = (nfin, reverse ss , reverse ns)
-
-
-
-
-  
-
+spaces :: Int -> String
+spaces n = replicate n ' '
 
 commas :: [String] -> String    
 commas = intercalate ", "
 
 re :: Complex a -> a
 re = realPart
+
 aim :: Num a => Complex a -> a
 aim = abs . imagPart
