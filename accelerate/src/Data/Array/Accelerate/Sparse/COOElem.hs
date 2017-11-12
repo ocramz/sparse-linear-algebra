52eabb9c1a404fee383f7db2b134b2c68f6c763b
ocramz <zocca.marco@gmail.com>
2017-11-12 10:41:24 +0100

    upd docs

--- accelerate/src/Data/Array/Accelerate/Sparse/COOElem.hs
+++ accelerate/src/Data/Array/Accelerate/Sparse/COOElem.hs
@@ -54,28 +54,29 @@ instance (Integral i, Storable i, Storable e) => VS.Storable (COOElem i e) where
   
 
 
--- `accelerate`-related instances  
+-- * `accelerate`-related instances  
 
 type instance EltRepr (COOElem i a) = EltRepr (i, i, a)
 
+-- | Elt
 instance (ArrayElt (EltRepr e), Typeable (EltRepr e), ArrayElt (EltRepr i), Typeable (EltRepr i), Show i, Show e, Typeable i, Typeable e, Elt i, Elt e) => A.Elt (COOElem i e) where
   fromElt (CooE (i, j, x)) = fromElt (i, j, x)
   toElt c = let (i, j, x) = toElt c in CooE (i, j, x)
   eltType (_ :: COOElem i a) = eltType (undefined :: (i, i, a))
 
+-- | IsProduct
 instance (Elt ix, Elt e) => IsProduct Elt (COOElem ix e) where
   type ProdRepr (COOElem ix e) = ((((), ix), ix), e)
   fromProd _ (CooE (i, j, x)) = ((((), i), j), x)
   toProd _ ((((),i), j), x) = CooE (i,j,x)
   prod cst _ = prod cst (undefined :: (i,i,e))
 
-
+-- | Lift
 instance (A.Lift A.Exp e, Elt (A.Plain e), A.Lift A.Exp i, Elt (A.Plain i)) => A.Lift A.Exp (COOElem i e) where
   type Plain (COOElem i e) = COOElem (A.Plain i) (A.Plain e)
   lift (CooE (i,j,x)) = Exp . Tuple $ NilTup `SnocTup` A.lift i `SnocTup` A.lift j `SnocTup` A.lift x
 
-
-
+-- | Unlift
 instance (A.Lift A.Exp e, Elt (A.Plain e), A.Lift A.Exp i, Elt (A.Plain i), Elt i, Elt e) => A.Unlift A.Exp (COOElem (A.Exp i) (A.Exp e)) where
   unlift c = let ii = Exp $ SuccTupIdx (SuccTupIdx ZeroTupIdx) `Prj` c
                  jj = Exp $ SuccTupIdx ZeroTupIdx `Prj` c
@@ -84,9 +85,3 @@ instance (A.Lift A.Exp e, Elt (A.Plain e), A.Lift A.Exp i, Elt (A.Plain i), Elt 
 
 
 
-
-
--- instance (Elt a, Elt b, Elt c) => Elt (a, b, c) where
---   eltType _             = PairTuple (eltType (undefined :: (a, b))) (eltType (undefined :: c))
---   fromElt (a, b, c)     = (fromElt (a, b), fromElt c)
---   toElt (ab, c)         = let (a, b) = toElt ab in (a, b, toElt c)
